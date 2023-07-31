ba7245271fb63eb4f4983aa7dc5e9f4a76e82bee
Nyasa0405 <105965435+Nyasa0405@users.noreply.github.com>
2023-07-31 11:13:19 +0900

    Feature_Falling

--- src/Assets/Scripts/PlayDirector.cs
+++ src/Assets/Scripts/PlayDirector.cs
@@ -9,6 +9,7 @@ interface IState
     {
         Control = 0,
         GameOver = 1,
+        Falling = 2,
 
         MAX,
 
@@ -19,11 +20,15 @@ interface IState
     E_State Update(PlayDirector parent);
 }
 
+[RequireComponent(typeof(BoardController))]
+
 public class PlayDirector : MonoBehaviour
 {
     [SerializeField] GameObject player = default!;
     PlayerController _playerController = null;
     LogicalInput _logicalInput = new();
+    BoardController _boardController = default!;
+
 
     NextQueue _nextQueue = new();
     [SerializeField] PuyoPair[] nextPuyoPairs = { default!, default! };// 次nextのゲームオブジェクトの制御
@@ -32,12 +37,14 @@ public class PlayDirector : MonoBehaviour
     static readonly IState[] states = new IState[(int)IState.E_State.MAX]{
         new ControlState(),
         new GameOverState(),
+          new FallingState(),
     };
 
     // Start is called before the first frame update
     void Start()
     {
         _playerController = player.GetComponent<PlayerController>();
+        _boardController = GetComponent<BoardController>();
         _logicalInput.Clear();
         _playerController.SetLogicalInput(_logicalInput);
 
@@ -99,10 +106,13 @@ public class PlayDirector : MonoBehaviour
             parent.UpdateNextsView();
             return IState.E_State.Unchanged;
         }
+
+
         public IState.E_State Update(PlayDirector parent)
         {
-            return parent.player.activeSelf ? IState.E_State.Unchanged : IState.E_State.Control;
+            return parent.player.activeSelf ? IState.E_State.Unchanged : IState.E_State.Falling;
         }
+
     }
 
     class GameOverState : IState
@@ -115,6 +125,17 @@ public class PlayDirector : MonoBehaviour
         public IState.E_State Update(PlayDirector parent) { return IState.E_State.Unchanged; }
     }
 
+    class FallingState : IState
+    {
+        public IState.E_State Initialize(PlayDirector parent)
+        {
+            return parent._boardController.CheckFall() ? IState.E_State.Unchanged : IState.E_State.Control;
+        }
+        public IState.E_State Update(PlayDirector parent)
+        {
+            return parent._boardController.Fall() ? IState.E_State.Unchanged : IState.E_State.Control;
+        }
+    }
     void InitializeState()
     {
         Debug.Assert(condition: _current_state is >= 0 and < IState.E_State.MAX);
